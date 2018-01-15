19d8a5cbdbd86e3df53360218777996601325e43
dk2233 <kucharski22daniel@gmail.com>
2018-01-15 11:08:36 +0000

    added reading from file all data and shows them each 2 s

--- ds1.py
+++ ds1.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 import time
@@ -6,14 +5,39 @@ import os
 
 ds_dir = "/sys/bus/w1/devices/"
 label = "28-"
+file_with_data = "w1_slave"
+
+
+
 
 tab_sensors = os.listdir(ds_dir)
 
 print(tab_sensors)
+for folder in tab_sensors:
+	if not label in folder:
+		continue
+	
+	file = open(ds_dir+folder+os.sep+file_with_data)
+	line = file.readline()
+	while line:
+		print( line)
+		line = file.readline()
+	file.close()	
+	
 
 
-
-#while(1):
-	#file=ope
+while(1):
+	for folder in tab_sensors:
+		if not label in folder:
+			continue
+		
+		file = open(ds_dir+folder+os.sep+file_with_data)
+		line = file.readline()
+		while line:
+			print( line)
+			line = file.readline()
+		file.close()	
+	print("-"*20)
+	time.sleep(2)
 
 
