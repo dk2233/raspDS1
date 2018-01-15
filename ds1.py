ea3d8430548f038b0a8731371eb074461ddde40c
dk2233 <kucharski22daniel@gmail.com>
2018-01-15 10:48:47 +0000

    initial2

---
+++ ds1.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python
+
+import time
+import os
+
+ds_dir = "/sys/bus/w1/devices/"
+label = "28-"
+
+tab_sensors = os.listdir(ds_dir)
+
+print(tab_sensors)
+
+
+
+#while(1):
+	#file=ope
+
+
